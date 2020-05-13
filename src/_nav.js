850bb90c5e26e8fbab52c1988321a82cfddbe49d
yosics <yosi.candra@gmail.com>
2020-05-13 21:54:18 +0700

    lntsrt

--- src/_nav.js
+++ src/_nav.js
@@ -1,82 +1,168 @@
 export default {
   items: [
     {
-      name: 'Compute',
+      name: 'App Management',
       url: '',
       icon: 'icon-grid4',
       children: [
         {
-          name: 'vApps',
+          name: 'Profile',
           url: '',
           icon: '',
         },
         {
-          name: 'Virtual Machines',
+          name: 'Authorization',
           url: '',
           icon: '',
         },
         {
-          name: 'Affinity Rules',
+          name: 'Services / Project Module',
+          url: '',
+          icon: '',
+        },
+        {
+          name: 'Audit Log',
+          url: '',
+          icon: '',
+        },
+        {
+          name: 'Operational',
           url: '',
           icon: '',
         },
       ]
     },
     {
-      name: 'Networking',
+      name: 'VMWare Infrastructure',
       url: '',
       icon: 'icon-sphere',
       children: [
         {
-          name: 'Networks',
+          name: 'Catalog',
           url: '',
           icon: '',
         },
         {
-          name: 'Edges',
+          name: 'Virtual Data Center',
           url: '',
           icon: '',
         },
         {
-          name: 'Security',
+          name: 'Virtual Machine',
+          url: '',
+          icon: '',
+        },
+        {
+          name: 'Network',
+          url: '',
+          icon: '',
+        },
+        {
+          name: 'Metrics',
           url: '',
           icon: '',
         },
       ]
     },
     {
-      name: 'Storage',
+      name: 'S3 StorageGrid',
       url: '',
       icon: 'icon-database4',
       children: [
         {
-          name: 'Independent Disks',
+          name: 'Catalogs',
           url: '',
           icon: '',
         },
         {
-          name: 'Storage Policies',
+          name: 'Users',
+          url: '',
+          icon: '',
+        },
+        {
+          name: 'Buckets',
           url: '',
           icon: '',
         },
       ]
     },
-    {
-      name: 'Setting',
-      url: '',
-      icon: 'icon-cog',
-      children: [
-        {
-          name: 'General',
-          url: '',
-          icon: '',
-        },
-        {
-          name: 'Metadata',
-          url: '',
-          icon: '',
-        },
-      ]
-    },
+    // {
+    //   name: 'Compute',
+    //   url: '',
+    //   icon: 'icon-grid4',
+    //   children: [
+    //     {
+    //       name: 'vApps',
+    //       url: '',
+    //       icon: '',
+    //     },
+    //     {
+    //       name: 'Virtual Machines',
+    //       url: '',
+    //       icon: '',
+    //     },
+    //     {
+    //       name: 'Affinity Rules',
+    //       url: '',
+    //       icon: '',
+    //     },
+    //   ]
+    // },
+    // {
+    //   name: 'Networking',
+    //   url: '',
+    //   icon: 'icon-sphere',
+    //   children: [
+    //     {
+    //       name: 'Networks',
+    //       url: '',
+    //       icon: '',
+    //     },
+    //     {
+    //       name: 'Edges',
+    //       url: '',
+    //       icon: '',
+    //     },
+    //     {
+    //       name: 'Security',
+    //       url: '',
+    //       icon: '',
+    //     },
+    //   ]
+    // },
+    // {
+    //   name: 'Storage',
+    //   url: '',
+    //   icon: 'icon-database4',
+    //   children: [
+    //     {
+    //       name: 'Independent Disks',
+    //       url: '',
+    //       icon: '',
+    //     },
+    //     {
+    //       name: 'Storage Policies',
+    //       url: '',
+    //       icon: '',
+    //     },
+    //   ]
+    // },
+    // {
+    //   name: 'Setting',
+    //   url: '',
+    //   icon: 'icon-cog',
+    //   children: [
+    //     {
+    //       name: 'General',
+    //       url: '',
+    //       icon: '',
+    //     },
+    //     {
+    //       name: 'Metadata',
+    //       url: '',
+    //       icon: '',
+    //     },
+    //   ]
+    // },
   ],
 };
